bd1f3659a8dfbf082b20bc59fdb4f03431feeb05
Claudia Calderas <claudia.calderas@razrhq.com>
2017-11-27 12:22:11 -0600

    Initial commit CRUD working

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyC3wPs5J3lSt5A1xiloQQt0v81l6jcJhKw",
+  authDomain: "angularfire-list-example-337c6.firebaseapp.com",
+  databaseURL: "https://angularfire-list-example-337c6.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "776366899885"
+};
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,131 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+    songs: AngularFireList<any>;
+    
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController,
+        public afDatabase: AngularFireDatabase, public actionSheetCtrl: ActionSheetController) {
+        this.songs = afDatabase.list('/songs/').valueChanges();
+    }
+    
+    addSong() {
+        let prompt = this.alertCtrl.create({
+            title: 'Song Name',
+            message: 'Enter a name for this new song',
+            inputs: [
+                {
+                    name: 'title',
+                    placeholder: 'Title'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => { console.log('Cancel clicked'); }
+                },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        // const newSongRef = this.songs.push({});
+                        // 
+                        // newSongRef.set ({
+                        //     id: newSongRef.key,
+                        //     title: data.title
+                        // });
+                        const newSongRef = this.afDatabase.list('/songs/').push({});
+                        
+                        newSongRef.set ({
+                             id: newSongRef.key,
+                             title: data.title
+                        });
+
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
+    
+    showOptions(songId, songTitle) {
+        console.log('songId',songId);
+        console.log('songTitle',songTitle);
+        let actionSheet = this.actionSheetCtrl.create({
+            title: 'What do you want to do?',
+            buttons: [
+                {
+                    text: 'Delete Song',
+                    role: 'destructive',
+                    handler: () => {
+                        this.removeSong(songId);
+                    }
+              },
+              {
+                text: 'Update title',
+                handler: () => {
+                  this.updateSong(songId, songTitle);
+                }
+              },
+              {
+                text: 'Cancel',
+                role: 'cancel',
+                handler: () => {
+                  console.log('Cancel clicked');
+                }
+              }
+            ]
+        });
+        actionSheet.present();
+    }
+    
+    removeSong(songId: string) {
+        // this.songs.remove(songId);
+        console.log('songId',songId);
+        this.afDatabase.list('/songs/').remove(songId);
+    }
+    
+    updateSong(songId,songTitle) {
+        let prompt = this.alertCtrl.create({
+            title: 'Song Name',
+            message: "Update the name for this song",
+            inputs: [
+                {
+                  name: 'title',
+                  placeholder: 'Title',
+                  value: songTitle
+                },
+            ],
+            buttons: [
+                {
+                  text: 'Cancel',
+                  handler: data => {
+                    console.log('Cancel clicked');
+                  }
+                },
+                {
+                  text: 'Save',
+                  handler: data => {
+                      
+                      
+                    // this.songs.update(songId, {
+                    //   title: data.title
+                    // });
+                    this.afDatabase.list('/songs/').update(songId, {
+                        title: data.title
+                    });
+                    
+                  }
+                }
+            ]
+        });
+        prompt.present();
+    }
+
+
+}
